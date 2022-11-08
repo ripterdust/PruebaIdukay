84f1042be4962807f04a3fc3d778f432b4c02b5c
Ripterdust <bryantello2010@hotmail.com>
2022-11-08 12:32:50 -0600

    :rocket: Arreglados los tests

--- src/test/operatingTime.test.ts
+++ src/test/operatingTime.test.ts
@@ -18,31 +18,129 @@ describe('Operating time - invalidation tests', () => {
 
 describe('Operating time - infinity cases', () => {
     test('First case', () => {
-        expect(calculateOperatingTime({0,0,0},{}, 100)).toBe(Infinity)
+        expect(
+            calculateOperatingTime(
+                {
+                    first: 0,
+                    second: 0,
+                    third: 0,
+                },
+                {
+                    first: 100,
+                    second: 100,
+                    third: 100,
+                },
+                100
+            )
+        ).toBe(Infinity)
     })
     test('Second case', () => {
-        expect(calculateOperatingTime(0, 0, 0, 90)).toBe(Infinity)
+        expect(
+            calculateOperatingTime(
+                {
+                    first: 0,
+                    second: 0,
+                    third: 0,
+                },
+                {
+                    first: 90,
+                    second: 90,
+                    third: 90,
+                },
+                90
+            )
+        ).toBe(Infinity)
     })
 
     test('Third case', () => {
-        expect(calculateOperatingTime(0, 0, 0, 30)).toBe(Infinity)
+        expect(
+            calculateOperatingTime(
+                {
+                    first: 0,
+                    second: 0,
+                    third: 0,
+                },
+                {
+                    first: 30,
+                    second: 30,
+                    third: 30,
+                },
+                30
+            )
+        ).toBe(Infinity)
     })
 })
 
 describe('Operating time - returns operating time in minutes', () => {
     test('Should return 90 minutes', () => {
-        expect(calculateOperatingTime(20, 10, 0, 100)).toBe(90)
+        expect(
+            calculateOperatingTime(
+                {
+                    first: 20,
+                    second: 10,
+                    third: 0,
+                },
+                {
+                    first: 90,
+                    second: 100,
+                    third: 110,
+                },
+                100
+            )
+        ).toBe(90)
     })
 
     test('Should return 80 minutes', () => {
-        expect(calculateOperatingTime(0, 0, 100, 80)).toBe(80)
+        expect(
+            calculateOperatingTime(
+                {
+                    first: 0,
+                    second: 0,
+                    third: 100,
+                },
+                {
+                    first: 120,
+                    second: 120,
+                    third: 0,
+                },
+                80
+            )
+        ).toBe(80)
     })
 
     test('Should return 50 minutes', () => {
-        expect(calculateOperatingTime(0, 0, 0, 150)).toBe(50)
+        expect(
+            calculateOperatingTime(
+                {
+                    first: 0,
+                    second: 0,
+                    third: 0,
+                },
+                {
+                    first: 150,
+                    second: 150,
+                    third: 150,
+                },
+                150
+            )
+        ).toBe(50)
     })
 
     test('Should return 50 minutes', () => {
-        expect(calculateOperatingTime(0, 0, 30, 140)).toBe(50)
+        expect(
+            calculateOperatingTime(
+                {
+                    first: 0,
+                    second: 0,
+                    third: 30,
+                },
+                {
+                    first: 150,
+                    second: 150,
+                    third: 120,
+                },
+                140
+            )
+        ).toBe(50)
     })
 })
--- src/utils/calculateOperatingTime.util.ts
+++ src/utils/calculateOperatingTime.util.ts
@@ -3,6 +3,5 @@ import { CalculateOperatingTime } from '../types/calculateOperatingTime.type'
 export const calculateOperatingTime: CalculateOperatingTime = (damage, fuel, cSpeed) => {
     if (fuel == noCompile) return 0
 
-    console.log(fuel)
     return 0
 }
