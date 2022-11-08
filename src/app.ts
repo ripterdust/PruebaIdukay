3649dc0429a823bad8acfeb33d3d4af217678207
Ripterdust <bryantello2010@hotmail.com>
2022-11-08 10:31:37 -0600

    :rocket: Agregada la funcionalidad principal de mostrar los datos en pantalla

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,9 @@ import { calculateOperatingTime } from './utils/calculateOperatingTime.util'
 export const results = (firstRocket: number, secondRocket: number, thirdRocket: number, cSpeed: number) => {
     const fuelNecesary = calculateMatter(firstRocket, secondRocket, thirdRocket, cSpeed)
     const operatingTime = calculateOperatingTime(firstRocket, secondRocket, thirdRocket, cSpeed)
+
     console.table({
-        'Necesary fuel': fuelNecesary,
+        'Fuel needed': fuelNecesary,
+        'Operating time': operatingTime,
     })
 }
