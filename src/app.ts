befd62afcb05845b80e9a03404975ada79106ede
Ripterdust <bryantello2010@hotmail.com>
2022-11-09 09:25:54 -0600

    :rocket: Agregado el comentario de la función de results

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,17 @@ import { calculateMatter } from './common/utils/calculateMatter.util'
 import { calculateOperatingTime } from './common/utils/calculateOperatingTime.util'
 import { getDataFromResponse } from './common/utils/transform.util'
 
+/**==============================================
+ **              results
+ *?  This function displays the result
+ *
+ *@param firstRocket number
+ *@param secondRocket number
+ *@param thirdRocket number
+ *@param cSpeed number
+ *@return null
+ *=============================================**/
+
 export const results = (firstRocket: number, secondRocket: number, thirdRocket: number, cSpeed: number) => {
     const fuelNecesary = calculateMatter(firstRocket, secondRocket, thirdRocket, cSpeed)
 
@@ -21,4 +32,6 @@ export const results = (firstRocket: number, secondRocket: number, thirdRocket: 
         'Operating time': operatingTime === Infinity ? operatingTime : `${operatingTime} minutes`,
         'Max speed': `${maxSpeed}% of light speed`,
     })
+
+    return null
 }
