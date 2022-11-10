7a7195e4f923ddaa2d59f4130d9b2153f2576d69
Ripterdust <bryantello2010@hotmail.com>
2022-11-10 13:01:37 -0600

    :rocket: Eliminada la arrow function abreviada

--- src/common/utils/get.util.ts
+++ src/common/utils/get.util.ts
@@ -1,4 +1,6 @@
-export const getMaxNumFromArray = (array: number[]) => array.reduce((acc, val) => (acc > val ? acc : val), 0)
+export const getMaxNumFromArray = (array: number[]) => {
+    return array.reduce((acc, val) => (acc > val ? acc : val), 0)
+}
 
 export const getMaxPercentage = (maxSpeed: number, maxDamaged: number): number => {
     return (maxSpeed / 100) * (100 - maxDamaged)
