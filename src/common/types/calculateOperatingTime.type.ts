5dbcebcc5577fb7d6475f48153c7895148d5cbe5
Ripterdust <bryantello2010@hotmail.com>
2022-11-10 12:51:20 -0600

    :rocket: Eliminados todos los ts-ignore y arreglada la parte de la función con el enter

--- src/common/types/calculateOperatingTime.type.ts
+++ src/common/types/calculateOperatingTime.type.ts
@@ -1,3 +1,3 @@
 import { Rockets } from '../interfaces/rocket.interface'
-import { noCompile } from '../misc/responses.response'
+
 export type CalculateOperatingTime = (damage: Rockets, fuel: Rockets | string, cSpeed: number) => number
--- src/common/utils/get.util.ts
+++ src/common/utils/get.util.ts
@@ -1,4 +1,5 @@
 export const getMaxNumFromArray = (array: number[]) => array.reduce((acc, val) => (acc > val ? acc : val), 0)
 
-export const getMaxPercentage = (maxSpeed: number, maxDamaged: number): number =>
-    (maxSpeed / 100) * (100 - maxDamaged)
+export const getMaxPercentage = (maxSpeed: number, maxDamaged: number): number => {
+    return (maxSpeed / 100) * (100 - maxDamaged)
+}
